ad656e58b219af8e5e38d4a25c2887e8c42ccdd5
XBird <zbyfoshan@163.com>
2021-04-17 15:25:28 +0800

    feat(mybatisplus活动练习):
    测试AR的查询操作

--- src/test/java/org/example/TestMp.java
+++ src/test/java/org/example/TestMp.java
@@ -10,7 +10,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 @RunWith(JUnit4.class)
 public class TestMp {
     private ApplicationContext ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
-    @Test
+
     public void testArInsert(){
         Employee employee=new Employee();
         employee.setLastName("宋老师");
@@ -19,5 +19,17 @@ public class TestMp {
         employee.setAge(35);
         boolean insert = employee.insert();
         System.out.println("插入结果："+insert);
+    }
+
+    @Test
+    public void testArUpdate(){
+        Employee employee=new Employee();
+        employee.setId(12);
+        employee.setLastName("宋老实");
+        employee.setEmail("sls@sina.com");
+        employee.setGender(1);
+        employee.setAge(36);
+        boolean b = employee.updateById();
+        System.out.println("更新结果："+b);
     }
 }
